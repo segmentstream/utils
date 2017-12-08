f62adbc54392ed8aab3c64a4993494f13b2a508a
Constantine Yurevich <yurevichcv@gmail.com>
2017-12-08 12:52:19 +0300

    dist

--- dist/isCrawler.js
+++ dist/isCrawler.js
@@ -2,7 +2,7 @@
 
 exports.__esModule = true;
 exports.default = isCrawler;
-var crawlers = [/bot/i, /spider/i, /facebookexternalhit/i, /simplepie/i, /yahooseeker/i, /embedly/i, /quora link preview/i, /outbrain/i, /vkshare/i, /monit/i, /Pingability/i, /Monitoring/i, /WinHttpRequest/i, /Apache-HttpClient/i, /getprismatic.com/i, /python-requests/i, /Twurly/i, /yandex/i, /browserproxy/i, /Monitoring/i, /crawler/i, /Qwantify/i, /Yahoo! Slurp/i, /pinterest/i];
+var crawlers = [/bot/i, /spider/i, /facebookexternalhit/i, /simplepie/i, /yahooseeker/i, /embedly/i, /quora link preview/i, /outbrain/i, /vkshare/i, /monit/i, /Pingability/i, /Monitoring/i, /WinHttpRequest/i, /Apache-HttpClient/i, /getprismatic.com/i, /python-requests/i, /Twurly/i, /compatible; yandex/i, /browserproxy/i, /Monitoring/i, /crawler/i, /Qwantify/i, /Yahoo! Slurp/i, /pinterest/i];
 
 function isCrawler(userAgent) {
   return crawlers.some(function (crawler) {
