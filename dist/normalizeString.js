10e19f12eb33c53145425be386541eee42a3f944
Svyatoslav Zadorozhnyy <svyatoslav@latoken.com>
2018-12-24 17:05:21 +0300

    Windows Comp
    change in package
    
    Fix package
    
    Add build script
    
    fix
    
    Add postinstall
    
    fix cpy
    
    Nice try
    
    add preinstall
    
    change to copy
    
    fix typo
    
    Test
    
    cpy
    
    prep
    
    add rm
    
    add npm run build
    
    Fixes

--- dist/normalizeString.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-exports.__esModule = true;
-exports.default = normalizeString;
-function normalizeString(str) {
-  if (!str) return '';
-
-  return String(str).trim().toLowerCase();
-}
