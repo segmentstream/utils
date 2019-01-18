2cde51ad58b54ab188eefd99fdd864edb0b0a39e
Kirill Temnov <allselead@gmail.com>
2019-01-18 18:14:22 +0300

    Another IE fix

--- src/helpers/setAttributes.js
+++ src/helpers/setAttributes.js
@@ -1,7 +1,7 @@
 export default (element, attributes, excludeKeys = []) => {
   Object.keys(attributes).forEach((key) => {
     const value = attributes[key];
-    if (!excludeKeys.includes(key)) {
+    if (excludeKeys.indexOf(key) < 0) {
       if (/^data-.*/.test(key)) {
         element.setAttribute(key, value);
       } else {
