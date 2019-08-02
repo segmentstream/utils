097c278c52f95c77d5660f4fd3063a9b0f060fa1
Evgeniy Kuznetsov <evgeny.kuznetsov@segmentstream.com>
2019-08-02 16:14:57 +0300

    Add test for size helper

---
+++ test/size/index.spec.js
@@ -0,0 +1,9 @@
+import { assert } from 'chai'
+import size from '../../src/size'
+
+describe('# size', () => {
+  it('should return right size of object', async () => {
+    const objSize = size({ a: 'a', b: false, c: {} })
+    assert.ok(objSize === 3)
+  })
+})
