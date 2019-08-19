886f8aba20d10bfe2cc61bf507c714bb1bea78b4
Evgeniy Kuznetsov <evgeny.kuznetsov@segmentstream.com>
2019-08-19 13:26:33 +0300

    Remove dist directory

--- dist/isPromise.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-exports.__esModule = true;
-exports.default = isPromise;
-
-function isPromise(result) {
-  return result && typeof result === 'object' && result.then && typeof result.then === 'function';
-}
